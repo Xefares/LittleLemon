488a5744048b698f9a13fa58be8bde7e5a4a6e33
unknown <bublitz30@gmail.com>
2024-12-29 09:45:34 -0700

    Added booking API

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from resturaunt import views
+router = routers.SimpleRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('resturaunt.urls')),
+    path('restaurant/', include('resturaunt.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
